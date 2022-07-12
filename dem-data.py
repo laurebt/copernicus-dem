8ee5ae8c70843618978c128f6f4b6074d33d7480
John <johnbt@runbox.com>
2022-07-11 22:01:47 -0600

    update

--- dem-data.py
+++ dem-data.py
@@ -530,9 +530,9 @@ def st_ui():
 
 
 if __name__ == "__main__":
-    # st_ui()
-    out = retrieve_dem(pre_defined_shape = ['World countries', 'Andorra'], return_type = 'dem')
-    print(out)
+    st_ui()
+    # out = retrieve_dem(pre_defined_shape = ['World countries', 'Andorra'], return_type = 'dem')
+    # print(out)
     # print(base64.b64encode(out))
 
 
